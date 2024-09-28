5c2a6460ef837454b7e65315260edc19657d03e7
IsaacLee-Ducky <isaac.david.lee.wai@gmail.com>
2024-09-28 07:45:49 +0000

    1234567890-

--- script/projects.js
+++ script/projects.js
@@ -1,1 +1,40 @@
-// 
+const canvasBoard = document.getElementById("CanvasBoard");
+let ctx = canvasBoard.getContext('2d');
+
+let X = 0
+let Y = 0
+
+let DX = 10;
+let DY = 10;
+
+let RADIUS = 10;
+
+let drawball = () => {
+    if(document.getElementById("playAnimation").checked){
+        ctx.reset();
+
+        ctx.beginPath();
+        ctx.arc(X, Y, RADIUS, 0, 2*Math.PI);
+        ctx.fill();
+        ctx.closePath();
+
+        if(X < 0 || X > canvasBoard.width){
+            DX = -DX;
+        }
+
+        if(Y < 0 || Y > canvasBoard.height){
+            DY = -DY
+
+            
+        }
+
+        X += DX;
+        Y += DY;
+    }
+        
+    requestAnimationFrame(drawball);
+
+};
+
+drawball();
+
